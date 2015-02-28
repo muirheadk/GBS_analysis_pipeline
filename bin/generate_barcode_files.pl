90d6db3dc07e89a8695b50e16786cc1244a053d8
cookeadmin <kevin5@ualberta.ca>
2015-02-28 14:51:56 -0700

    Updated generate_barcode_files.pl on 2015-02-28. Revised the code so that a flowcell name is specified in the first column and a plate number in the second column. The rest of the GBS shipment file is ordered the same as previous versions of this script. You can have as many flowcell/plates in the same GBS shipment file and an number of individuals. So that we can generate multiple barcode files at any given time as input to the fastq_quality_barcode_splitter.pl script. Incorporated code to fill in missing wells if they are not present in the GBS shipment file given as input. Replaces wells with miscellaneous sample names so that process_radtags can properly demultiplex the raw compressed fastq files.

--- bin/generate_barcode_files.pl
+++ bin/generate_barcode_files.pl
@@ -11,11 +11,10 @@ use Getopt::Long;
 
 #### SAMPLE COMMAND ####
 # perl generate_barcode_files.pl -i ~/workspace/GBS_data-08-10-2013/GBS_shipment_9Oct2013.csv -l ~/workspace/GBS_data-08-10-2013/Pstl_MspI_barcodes_legend.csv -o ~/workspace/GBS_data-08-10-2013/GBS_BARCODES
-my ($GBS_shipment_infile, $barcodes_legend_infile, $flowcell_name, $barcodes_output_dir);
+my ($GBS_shipment_infile, $barcodes_legend_infile, $barcodes_output_dir);
 GetOptions(
 	'i=s'    => \$GBS_shipment_infile, # The GBS shipment file in tab-delimited format (i.e. GBS_shipment_9Oct2013.csv)
 	'l=s'    => \$barcodes_legend_infile, # The barcodes legend file in tab-delimited format (i.e. Pstl_MspI_barcodes_legend.csv)
-	'f=s'    => \$flowcell_name, # The flow cell name used for the TASSEL GBS variant pipeline barcodes file format. Default: GQ03122013
 	'o=s'    => \$barcodes_output_dir,  # The absolute path to the output directory to contain the barcodes output files
 );
 
@@ -27,7 +26,7 @@ usage() unless (
 );
 
 # The flow cell name used for the TASSEL GBS variant pipeline barcodes file format.
-$flowcell_name = "GQ03122013" unless defined $flowcell_name;
+# $flowcell_name = "GQ03122013" unless defined $flowcell_name;
 
 sub usage {
     
@@ -46,8 +45,6 @@ OPTIONS:
 -l barcodes_legend_infile - The barcodes legend file in tab-delimited format (i.e. Pstl_MspI_barcodes_legend.csv)
 
 	e.g. /path/to/Pstl_MspI_barcodes_legend.csv
-
--f flowcell_name - The flow cell name used for the TASSEL GBS variant pipeline barcodes file format. Default: GQ03122013
 
 -o barcodes_output_dir - The absolute path to the output directory to contain the barcodes output files
 
@@ -72,28 +69,60 @@ unless(-d $barcodes_output_dir){
 # 1	E1	PIS-7	25.12	1.8	1.92	15.7	Greg Breed
 # 1	F1	PIS-12	36.28	1.74	1.61	33	Greg Breed
 # 1	G1	HARV-17L	22.81	1.72	1.71	17.3	Greg Breed
-my %GBS_samples = ();
+my %gbs_shipment_entries = ();
 open(INFILE, "<$GBS_shipment_infile") or die "Couldn't open file $GBS_shipment_infile for reading, $!";
 my $i = 0;
 while(<INFILE>){
 	chomp $_;
-	#    warn "$_\n";
-	if($i ne 0){ # Skip the GBS shipment header because we know the positions of the contents within the file.
 	
+	if($i ne 0){ # Skip the GBS shipment header because we know the positions of the contents within the file.
+		my @split_gbs_shipment_entry = split(/\t/, $_);
+		my ($flowcell_name, $plate_num, $well_num, $run_id, $nandrop, $f260_280, $f260_230, $qubit, $project_leader) = @split_gbs_shipment_entry;
+		
+		my $flowcell_plate_name = join("_", $flowcell_name, $plate_num);
+  		$gbs_shipment_entries{$flowcell_plate_name}{$well_num} = join("\t", $flowcell_name, $plate_num, $well_num, $run_id, $nandrop, $f260_280, $f260_230, $qubit, $project_leader);
+	}
+	$i++;
+}
+close(INFILE) or die "Couldn't close file $GBS_shipment_infile";
+
+# We want to fill in empty wells with miscellaneous data if they are present in the GBS shipment file.
+my @alpha_well_codes = ("A","B","C","D","E","F","G","H");
+my @alpha_numeric_well_codes = ();
+for(my $i = 1; $i <= 12; $i++){
+	foreach my $alpha_well_code (@alpha_well_codes){
+# 		warn join("", $alpha_well_code, $i);
+		push(@alpha_numeric_well_codes, join("", $alpha_well_code, $i));
+	}
+}
+
+foreach my $flowcell_plate_name (sort keys %gbs_shipment_entries){
+	warn $flowcell_plate_name . "\n";
+	foreach my $well_num (@alpha_numeric_well_codes){
+		if(!defined($gbs_shipment_entries{$flowcell_plate_name}{$well_num})){
+			my @split_flowcell_plate_name = split(/_/, $flowcell_plate_name);
+			my ($flowcell_name, $plate_name) = @split_flowcell_plate_name;
+			$gbs_shipment_entries{$flowcell_plate_name}{$well_num} = join("\t", $flowcell_name, $plate_name, $well_num, join("_", "miscellaneous", $flowcell_plate_name, $well_num) , 0, 0, 0, 0, join("_", "miscellaneous", $flowcell_plate_name));
+		}
+	}
+}
+
+my %GBS_samples = ();
+foreach my $flowcell_plate_name (sort keys %gbs_shipment_entries){
+# 	warn $flowcell_plate_name . "\n";
+	foreach my $well_num (@alpha_numeric_well_codes){
 		# Split the contents of the GBS shipment file into the plate number, well number, sample/individual id, and the name of the project leader.
 		# We are only interested in these values of the file so that we can concatenate based on the well alphanumeric code. 
-		my @split_entries = split(/\t/, $_);
-		my ($plate_num, $well_num, $sample_id, $project_leader_name) = ($split_entries[0], $split_entries[1], $split_entries[2], $split_entries[7]);
+		my @split_entries = split(/\t/, $gbs_shipment_entries{$flowcell_plate_name}{$well_num});
+		my ($flowcell_name, $plate_num, $well_num, $sample_id, $project_leader_name) = ($split_entries[0], $split_entries[1], $split_entries[2], $split_entries[3], $split_entries[8]);
 		
 		# Store each well number, sample/individual id, and project leader entry into a hash array indexed by the plate number. 
 		# We want to generate barcode output files based on plate number. Once we have the files we can sort by project leader or grep based 
 		# on the sample/individual ids of interest.
-		push(@{$GBS_samples{$plate_num}}, join("\t", $sample_id, $well_num, $project_leader_name));
+		my $flowcell_plate_name = join("_", $flowcell_name, $plate_num);
+		push(@{$GBS_samples{$flowcell_plate_name}}, join("\t", $sample_id, $well_num, $project_leader_name));
 	}
-	$i++;
-    
 }
-close(INFILE) or die "Couldn't close file $GBS_shipment_infile";
 
 # Parse the barcodes legend file in tab-delimited format (i.e. Pstl_MspI_barcodes_legend.csv) to generate a hash variable that indexes 
 # barcodes based on the well alphanumeric code. 
@@ -135,43 +164,6 @@ while(<INFILE>){
 }
 close(INFILE) or die "Couldn't close file $barcodes_legend_infile";
 
-# Create the TASSEL barcode output directory if it doesn't already exist.
-my $tassel_barcodes_output_dir = join('/', $barcodes_output_dir, "TASSEL_BARCODE_OUTFILES");
-unless(-d $tassel_barcodes_output_dir){
-    mkdir($tassel_barcodes_output_dir, 0777) or die "Can't make directory: $!";
-}
-
-# Generate TASSEL GBS variant pipeline barcodes file format.
-# Concatenates the contents of the GBS shipment file and barcodes legend file based on well alphanumeric code and separates based on plate number.
-# An example of the TASSEL GBS variant pipeline barcodes file format.
-# Flowcell        Lane    Barcode Sample  PlateName       Row     Column
-# GQ03122013      5       AGCG    M033-11-02-05-DA41      plate5  A       2
-# Iterate through each GBS plate number and print the barcode entry based on the GBS shipment and barcode legend files.
-foreach my $plate_num (sort keys %GBS_samples){
-
-	# Get the barcode output file path and open the file for writting the barcode entries for each GBS plate.
-	my $plate_name = join("_", "plate", $plate_num);
-	my $tassel_barcodes_outfile = join('/', $tassel_barcodes_output_dir, join("-", "tassel-barcodes", $plate_name . ".txt"));
-	open(OUTFILE, ">$tassel_barcodes_outfile") or die "Couldn't open file $tassel_barcodes_outfile for writting, $!";
-	print OUTFILE join("\t", "Flowcell", "Lane", "Barcode", "Sample", "PlateName", "Row", "Column") . "\n";
-	
-	# Iterate through each GBS sample entry and print the barcode entry.
-	foreach my $GBS_sample (@{$GBS_samples{$plate_num}}){
-	
-		# Split the GBS sample entry and parse for the sample id, alpha-numeric well number, and project leader.
-		my @split_GBS_samples = split(/\t/, $GBS_sample);
-		my ($sample_id, $well_num, $project_leader) = @split_GBS_samples;
-		
-		# Split the well alpha numeric code and obtain the alpha and number codes separately.
-		my ($well_alpha_code, $well_num_code) = split('', $well_num);
-
-		# Print the barcode entry.
-		print OUTFILE join("\t", $flowcell_name, $plate_num, $GBS_barcodes_legend{$well_alpha_code}{$well_num_code}, $sample_id, $plate_name, $well_alpha_code, $well_num_code) . "\n";
-	}
-	close(OUTFILE) or die "Couldn't close file $tassel_barcodes_outfile";
-    
-}
-
 # Create the Stacks GBS variant pipeline barcodes output directory if it doesn't already exist.
 my $stacks_barcodes_output_dir = join('/', $barcodes_output_dir, "STACKS_BARCODE_OUTFILES");
 unless(-d $stacks_barcodes_output_dir){
@@ -184,16 +176,16 @@ unless(-d $stacks_barcodes_output_dir){
 # plate_number    well_row        well_column     run_id  project_leader_name  barcode_sequence
 # 5       A       2       M033_11_02_05_DA41      JASMINE_JANES   AGCG
 # Iterate through each GBS plate number and print the barcode entry based on the GBS shipment and barcode legend files.
-foreach my $plate_num (sort keys %GBS_samples){
+foreach my $flowcell_plate_name (sort keys %GBS_samples){
 
 	# Get the barcode output file path and open the file for writting the barcode entries for each GBS plate.
-	my $plate_name = join("_", "plate", $plate_num);
-	my $barcodes_outfile = join('/', $stacks_barcodes_output_dir, join("-", "stacks-barcodes", $plate_name . ".txt"));
+	my $barcodes_outfile = join('/', $stacks_barcodes_output_dir, join("-", $flowcell_plate_name,  "barcodes.txt"));
 	open(OUTFILE, ">$barcodes_outfile") or die "Couldn't open file $barcodes_outfile for writting, $!";
-	print OUTFILE join("\t", "plate_number", "well_row", "well_column", "run_id", "project_leader_name", "barcode_sequence") . "\n";
+	print OUTFILE join("\t", "flowcell_name", "plate_number", "well_row", "well_column", "run_id", "project_leader_name", "barcode_sequence") . "\n";
 	
 	# Iterate through each GBS sample entry and print the barcode entry.
-	foreach my $GBS_sample (@{$GBS_samples{$plate_num}}){
+	foreach my $GBS_sample (@{$GBS_samples{$flowcell_plate_name}}){
+	
 		# Split the GBS sample entry and parse for the sample id, alpha-numeric well number, and project leader.
 		my @split_GBS_samples = split(/\t/, $GBS_sample);
 		my ($sample_id, $well_num, $project_leader_name) = @split_GBS_samples;
@@ -211,8 +203,11 @@ foreach my $plate_num (sort keys %GBS_samples){
 		$project_leader_name =~ s/ /_/g;
 		$project_leader_name = uc($project_leader_name);
 
+		my @split_flowcell_plate_name = split(/_/, $flowcell_plate_name);
+		my ($flowcell_name, $plate_num) = @split_flowcell_plate_name;
+		
 		# Print the barcode entry.
-		print OUTFILE join("\t", $plate_num, $well_alpha_code, $well_num_code, $sample_id, $project_leader_name, $GBS_barcodes_legend{$well_alpha_code}{$well_num_code}) . "\n";
+		print OUTFILE join("\t", $flowcell_name, $plate_num, $well_alpha_code, $well_num_code, $sample_id, $project_leader_name, $GBS_barcodes_legend{$well_alpha_code}{$well_num_code}) . "\n";
 	}
 	close(OUTFILE) or die "Couldn't close file $barcodes_outfile";
 }
