0dcc0027445dca9d450da70b17d5b82b6a9cf259
muirheadk <root@Kevins-MacBook-Pro.local>
2015-06-11 00:14:48 -0600

    Updated generate_barcode_files.pl on 2015-06-10. Just commiting changes likely just extra comments.

--- bin/generate_barcode_files.pl
+++ bin/generate_barcode_files.pl
@@ -10,18 +10,18 @@ use Getopt::Long;
 # This program generates barcode input files for input into the Stacks fastq_quality_barcode_splitter.pl script. Takes a GBS shipment and barcode legend file as input and generates a barcode file for each GBS plate listed in the GBS shipment file.
 
 #### SAMPLE COMMAND ####
-# perl generate_barcode_files.pl -i ~/workspace/GBS_data-08-10-2013/GBS_Shipment_GQ03122013_5.txt -l ~/workspace/GBS_data-08-10-2013/Pstl_MspI_barcodes_legend.txt -o ~/workspace/GBS_data-08-10-2013/GBS_BARCODES
+# perl generate_barcode_files.pl -i ~/workspace/GBS_data-08-10-2013/GBS_Shipment_GQ03122013_5.txt -l ~/workspace/GBS_data-08-10-2013/PstI_MspI_barcodes_legend.txt -o ~/workspace/GBS_data-08-10-2013/GBS_BARCODES
 my ($GBS_shipment_infile, $barcodes_legend_infile, $barcodes_output_dir);
 GetOptions(
 	'i=s'    => \$GBS_shipment_infile, # The GBS shipment file in tab-delimited format (i.e. GBS_Shipment_GQ03122013_5.txt)
-	'l=s'    => \$barcodes_legend_infile, # The barcodes legend file in tab-delimited format (i.e. Pstl_MspI_barcodes_legend.txt)
+	'l=s'    => \$barcodes_legend_infile, # The barcodes legend file in tab-delimited format (i.e. PstI_MspI_barcodes_legend.txt)
 	'o=s'    => \$barcodes_output_dir,  # The absolute path to the output directory to contain the barcodes output files
 );
 
 # Display a usage message if the following parameters are not specified.
 usage() unless (
-	defined $GBS_shipment_infile
-	and defined $barcodes_legend_infile
+    defined $GBS_shipment_infile
+    and defined $barcodes_legend_infile
 	and defined $barcodes_output_dir
 );
 
@@ -31,7 +31,7 @@ die <<"USAGE";
     
 Usage: $0 -i GBS_shipment_infile -l barcodes_legend_infile -o barcodes_output_dir
     
-DESCRIPTION - This program generates barcode input files for input into the Stacks fastq_quality_barcode_splitter.pl script. Takes a GBS shipment and barcode legend file as input and generates a barcode file for each GBS plate listed in the GBS shipment file.
+DESCRIPTION - This program generates barcode input files for input into the Stacks fastq_quality_barcode_splitter.pl script. Takes a GBS shipment file and barcode legend file as input. Generates a barcode file for each GBS plate listed in the GBS shipment file.
 
 OPTIONS:
 
@@ -39,9 +39,9 @@ OPTIONS:
 
 	e.g. /path/to/GBS_Shipment_GQ03122013_5.txt
 
--l barcodes_legend_infile - The barcodes legend file in tab-delimited format (i.e. Pstl_MspI_barcodes_legend.txt)
+-l barcodes_legend_infile - The barcodes legend file in tab-delimited format (i.e. PstI_MspI_barcodes_legend.txt)
 
-	e.g. /path/to/Pstl_MspI_barcodes_legend.txt
+	e.g. /path/to/PstI_MspI_barcodes_legend.txt
 
 -o barcodes_output_dir - The absolute path to the output directory to contain the barcodes output files
 
@@ -59,18 +59,18 @@ unless(-d $barcodes_output_dir){
 # well number, sample/individual id, and the name of the project leader based on the plate number.
 # GBS shipment file format from GBS_Shipment_GQ03122013_5.txt.
 # Flowcell_name	Plate #	Well #	ID #	Nanodrop ng/ul	260/280	260/230	Qubit ng/ul	Project Leader
-# GQ03122013	5	A2	M033-11-02-05-DA41	446.6	2.1	2.59	26.1	Stephen Trevoy
-# GQ03122013	5	B2	M033-06-01-05-DA126	739.4	2.12	2.04	27.8	Stephen Trevoy
-# GQ03122013	5	C2	M033-07-01-09-DA36	542.3	2.08	2.46	30.3	Stephen Trevoy
-# GQ03122013	5	D2	M004-13-01-1D-G14-DA01	225.2	2.19	2.04	15.2	Stephen Trevoy
-# GQ03122013	5	E2	M004-13-01-5ST-G36-DA02	233.5	2.2	2.23	12.8	Stephen Trevoy
-# GQ03122013	5	F2	M004-13-01-2F-G24-DA01	242.4	2.18	2.1	14.8	Stephen Trevoy
-# GQ03122013	5	G2	M004-13-01-2D-G05-DA01	209.5	1.96	1.2	16.7	Stephen Trevoy
-# GQ03122013	5	H2	M024-07-01-02-DA07	1396	2.14	1.9	20	Stephen Trevoy
-# GQ03122013	5	A3	LL-06	181.6	2.14		20	Stephen Trevoy
-# GQ03122013	5	B3	NG-38	182.2	2.06		20	Stephen Trevoy
-# GQ03122013	5	C3	RR-45	124.2	2.15		20	Stephen Trevoy
-# GQ03122013	5	D3	QU-48	137.9	2.22		20	Stephen Trevoy
+# GQ03122013	5	A2	M033-11-02-05-DA41	446.6	2.1	2.59	26.1	John Smith
+# GQ03122013	5	B2	M033-06-01-05-DA126	739.4	2.12	2.04	27.8	John Smith
+# GQ03122013	5	C2	M033-07-01-09-DA36	542.3	2.08	2.46	30.3	John Smith
+# GQ03122013	5	D2	M004-13-01-1D-G14-DA01	225.2	2.19	2.04	15.2	John Smith
+# GQ03122013	5	E2	M004-13-01-5ST-G36-DA02	233.5	2.2	2.23	12.8	John Smith
+# GQ03122013	5	F2	M004-13-01-2F-G24-DA01	242.4	2.18	2.1	14.8	John Smith
+# GQ03122013	5	G2	M004-13-01-2D-G05-DA01	209.5	1.96	1.2	16.7	John Smith
+# GQ03122013	5	H2	M024-07-01-02-DA07	1396	2.14	1.9	20	John Smith
+# GQ03122013	5	A3	LL-06	181.6	2.14		20	John Smith
+# GQ03122013	5	B3	NG-38	182.2	2.06		20  John Smith
+# GQ03122013	5	C3	RR-45	124.2	2.15		20	John Smith
+# GQ03122013	5	D3	QU-48	137.9	2.22		20	John Smith
 my %gbs_shipment_entries = ();
 open(INFILE, "<$GBS_shipment_infile") or die "Couldn't open file $GBS_shipment_infile for reading, $!";
 my $i = 0;
@@ -112,28 +112,29 @@ foreach my $flowcell_plate_name (sort keys %gbs_shipment_entries){
 	}
 }
 
+# Iterate through each well of each plate based on flowcell name and plate number.
 my %GBS_samples = ();
 foreach my $flowcell_plate_name (sort keys %gbs_shipment_entries){
 # 	warn $flowcell_plate_name . "\n";
 	foreach my $well_num (@alpha_numeric_well_codes){
-		# Split the contents of the GBS shipment file into the plate number, well number, sample/individual id, and the name of the project leader.
+		# Split the contents of the GBS shipment file into the flowcell name, plate number, well number, sample/individual id, and the name of the project leader.
 		# We are only interested in these values of the file so that we can concatenate based on the well alphanumeric code. 
 		my @split_entries = split(/\t/, $gbs_shipment_entries{$flowcell_plate_name}{$well_num});
 		my ($flowcell_name, $plate_num, $well_num, $sample_id, $project_leader_name) = ($split_entries[0], $split_entries[1], $split_entries[2], $split_entries[3], $split_entries[8]);
 		
-		# Store each well number, sample/individual id, and project leader entry into a hash array indexed by the plate number. 
-		# We want to generate barcode output files based on plate number. Once we have the files we can sort by project leader or grep based 
+		# Store each well number, sample/individual id, and project leader entry into a hash array indexed by the flowcell name and plate number.
+		# We want to generate barcode output files based on flowcell name and plate number. Once we have the files we can sort by project leader or grep based
 		# on the sample/individual ids of interest.
 		my $flowcell_plate_name = join("_", $flowcell_name, $plate_num);
 		push(@{$GBS_samples{$flowcell_plate_name}}, join("\t", $sample_id, $well_num, $project_leader_name));
 	}
 }
 
-# Parse the barcodes legend file in tab-delimited format (i.e. Pstl_MspI_barcodes_legend.txt) to generate a hash variable that indexes 
+# Parse the barcodes legend file in tab-delimited format (i.e. PstI_MspI_barcodes_legend.txt) to generate a hash variable that indexes 
 # barcodes based on the well alphanumeric code. 
 # i.e. {A}{1} is well number A1 and stores barcode sequence "CTCG".
 #
-# Barcodes legend format from Pstl_MspI_barcodes_legend.txt. Taken from the PstI/MspI portion of the Identifiants RAD.xlsx excel file.
+# Barcodes legend format from PstI_MspI_barcodes_legend.txt. Taken from the PstI/MspI portion of the Identifiants RAD.xlsx excel file.
 # 	1	2	3	4	5	6	7	8	9	10	11	12
 # A	CTCG	AGCG	TTCTG	ATTGA	TCGTT	GAGATA	CTATTA	CTTGCTT	AATATGG	GCGGAAT	TGCAAGGA	CCATGGGT
 # B	TGCA	GATG	AGCCG	CATCT	GGTTGT	ATGCCT	GCCAGT	ATGAAAG	ACGTGTT	TAGCGGA	TGGTACGT	CGCGGAGA
@@ -179,18 +180,18 @@ unless(-d $stacks_barcodes_output_dir){
 # Concatenates the contents of the GBS shipment file and barcodes legend file based on well alphanumeric code and separates based on plate number.
 # An example of the barcodes file format.
 # flowcell_name	plate_number	well_row	well_column	run_id	project_leader_name	barcode_sequence
-# GQ03122013	5	A	2	M033-11-02-05-DA41	STEPHEN_TREVOY	AGCG
-# GQ03122013	5	B	2	M033-06-01-05-DA126	STEPHEN_TREVOY	GATG
-# GQ03122013	5	C	2	M033-07-01-09-DA36	STEPHEN_TREVOY	TCAG
-# GQ03122013	5	D	2	M004-13-01-1D-G14-DA01	STEPHEN_TREVOY	TGCGA
-# GQ03122013	5	E	2	M004-13-01-5ST-G36-DA02	STEPHEN_TREVOY	CGCTT
-# GQ03122013	5	F	2	M004-13-01-2F-G24-DA01	STEPHEN_TREVOY	TCACG
-# GQ03122013	5	G	2	M004-13-01-2D-G05-DA01	STEPHEN_TREVOY	CTAGG
-# GQ03122013	5	H	2	M024-07-01-02-DA07	STEPHEN_TREVOY	ACAAA
-# GQ03122013	5	A	3	LL-06	STEPHEN_TREVOY	TTCTG
-# GQ03122013	5	B	3	NG-38	STEPHEN_TREVOY	AGCCG
-# GQ03122013	5	C	3	RR-45	STEPHEN_TREVOY	GTATT
-# GQ03122013	5	D	3	QU-48	STEPHEN_TREVOY	CTGTA
+# GQ03122013	5	A	2	M033-11-02-05DA41	JOHN_SMITH	AGCG
+# GQ03122013	5	B	2	M033-06-01-05-DA126	JOHN_SMITH	GATG
+# GQ03122013	5	C	2	M033-07-01-09-DA36	JOHN_SMITH	TCAG
+# GQ03122013	5	D	2	M004-13-01-1D-G14-DA01	JOHN_SMITH	TGCGA
+# GQ03122013	5	E	2	M004-13-01-5ST-G36-DA02	JOHN_SMITH	CGCTT
+# GQ03122013	5	F	2	M004-13-01-2F-G24-DA01	JOHN_SMITH	TCACG
+# GQ03122013	5	G	2	M004-13-01-2D-G05-DA01	JOHN_SMITH	CTAGG
+# GQ03122013	5	H	2	M024-07-01-02-DA07	JOHN_SMITH	ACAAA
+# GQ03122013	5	A	3	LL-06	JOHN_SMITH	TTCTG
+# GQ03122013	5	B	3	NG-38	JOHN_SMITH	AGCCG
+# GQ03122013	5	C	3	RR-45	JOHN_SMITH	GTATT
+# GQ03122013	5	D	3	QU-48	JOHN_SMITH	CTGTA
 # Iterate through each GBS plate number and print the barcode entry based on the GBS shipment and barcode legend files.
 foreach my $flowcell_plate_name (sort keys %GBS_samples){
 
@@ -212,8 +213,8 @@ foreach my $flowcell_plate_name (sort keys %GBS_samples){
 			($well_alpha_code, $well_num_code) = ($1, $2);
 		}
 
-		# Replace any spaces in the sample id with an underscore.
-		$sample_id =~ s/ /-/g;
+		# Replace any spaces or hyphens in the sample id with an underscore.
+		$sample_id =~ s/(-| )/_/g;
 
 		# Replace any spaces in the project leader name with an underscore and convert to uppercase if not already done.
 		$project_leader_name =~ s/ /_/g;
