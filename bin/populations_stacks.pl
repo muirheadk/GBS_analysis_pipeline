ac5d4b7f0e00a1de42ce918573d819a3e4fe7287
cookeadmin <kevin5@ualberta.ca>
2015-07-01 05:33:51 -0600

    Updated populations_stacks.pl on 2015-06-30. Added much needed parameter and usage comments. Now shows program defaults.

--- bin/populations_stacks.pl
+++ bin/populations_stacks.pl
@@ -10,15 +10,15 @@ use File::Copy;
 
 my ($stacks_dir, $pop_map_infile, $batch_num, $min_num_pops_locus, $min_percent_indvs_pop, $min_stack_depth, $min_allele_freq, $outfile_type, $num_cpu_cores, $output_dir);
 GetOptions(
-	'P=s'    => \$stacks_dir,
-	'M=s'    => \$pop_map_infile,
-	'b=s'    => \$batch_num,
-	'p=s'    => \$min_num_pops_locus,
-	'r=s'    => \$min_percent_indvs_pop,
-	'm=s'    => \$min_stack_depth,
-	'a=s'    => \$min_allele_freq,
-	'f=s'    => \$outfile_type,
-	't=s'    => \$num_cpu_cores,
+	'P=s'    => \$stacks_dir, # The absolute path to the Stacks output files.
+	'M=s'    => \$pop_map_infile, # The  absolute path to the population map, a tab separated file describing which individuals belong in which population.
+	'b=s'    => \$batch_num, # The batch ID to examine when exporting from the catalog.
+	'p=s'    => \$min_num_pops_locus, # The minimum number of populations a locus must be present in to process a locus. Default: 2
+	'r=s'    => \$min_percent_indvs_pop, # The minimum percentage of individuals in a population required to process a locus for that population.  Default: 0
+	'm=s'    => \$min_stack_depth, # The minimum stack depth required for individuals at a locus. Default: 10
+	'a=s'    => \$min_allele_freq, # The minimum minor allele frequency required before calculating Fst at a locus (0 < a < 0.5). Default: 0.05
+	'f=s'    => \$outfile_type, # The output file format type. Outputs the full sequence for each allele, from each sample locus in FASTA format, Population structure for each allele, from each sample locus in Structure format, etc. Can be either fasta, structure, fasta_single_snp, structure_single_snp, write_singleDefault: 'fasta'
+	't=s'    => \$num_cpu_cores, # The number of threads to run in parallel sections of code. Default: 2
 	'o=s'    => \$output_dir, # The absolute path to the output directory to contain the Stacks output files and directories.
 );
 
@@ -28,18 +28,25 @@ usage() unless (
 	and defined $output_dir
 );
 
+# The batch ID to examine when exporting from the catalog.
 $batch_num = 1 unless defined $batch_num;
 
+# The minimum number of populations a locus must be present in to process a locus. Default: 2
 $min_num_pops_locus = 2 unless defined $min_num_pops_locus;
 
+# The minimum percentage of individuals in a population required to process a locus for that population. Default: 0
 $min_percent_indvs_pop = 0 unless defined $min_percent_indvs_pop;
 
+# The minimum stack depth required for individuals at a locus. Default: 10
 $min_stack_depth = 10 unless defined $min_stack_depth;
 
+# The minimum minor allele frequency required before calculating Fst at a locus (0 < a < 0.5). Default: 0.05
 $min_allele_freq = 0.05 unless defined $min_allele_freq;
 
+# The number of threads to run in parallel sections of code. Default: 2
 $num_cpu_cores = 2 unless defined $num_cpu_cores;
 
+# The output file format type. Outputs the full sequence for each allele, from each sample locus in FASTA format, Population structure for each allele, from each sample locus in Structure format, etc. Can be either fasta, structure, fasta_single_snp, structure_single_snp, write_singleDefault: 'fasta'
 $outfile_type = 'fasta' unless defined $outfile_type;
 
 # Program dependencies - The stacks populations program.
@@ -49,32 +56,29 @@ sub usage {
 
 die <<"USAGE";
 
-Usage: $0 -P stacks_dir -M pop_map_infile -b batch_num -p min_num_pops_locus -r min_percent_indvs_pop -m min_stack_depth -a min_allele_freq -t num_cpu_cores -f outfile_type -o output_dir
-
-DESCRIPTION -
+DESCRIPTION – Program that executes the Stacks populations program that analyzes a population of individual samples given a population map file that designates what individual belongs to what population. The populations program computes a number of population genetics statistics as well as exporting a variety of standard output formats such as fasta and structure formats.
 
 OPTIONS:
 
--P stacks_dir - The  absolute path to the Stacks output files.
+-P stacks_dir - The absolute path to the Stacks output files.
 
 -M pop_map_infile - The  absolute path to the population map, a tab separated file describing which individuals belong in which population.
 
 -b batch_num - The batch ID to examine when exporting from the catalog.
 
--p min_num_pops_locus - The minimum percentage of individuals in a population required to process a locus for that population.
+-p min_num_pops_locus - The minimum number of populations a locus must be present in to process a locus. Default: 2
 
--r min_percent_indvs_pop - The minimum number of populations a locus must be present in to process a locus.
+-r min_percent_indvs_pop - The minimum percentage of individuals in a population required to process a locus for that population.  Default: 0
 
--m min_stack_depth - The minimum stack depth required for individuals at a locus.
+-m min_stack_depth - The minimum stack depth required for individuals at a locus. Default: 10
 
--a min_allele_freq - The minimum minor allele frequency required before calculating Fst at a locus (0 < a < 0.5).
+-a min_allele_freq - The minimum minor allele frequency required before calculating Fst at a locus (0 < a < 0.5). Default: 0.05
 
--f outfile_type - fasta - output full sequence for each allele, from each sample locus in FASTA format. structure - output results in Structure format. struct_single_snp write single snp for structure
+-f outfile_type – The output file format type. Outputs the full sequence for each allele, from each sample locus in FASTA format, Population structure for each allele, from each sample locus in Structure format, etc. Can be either fasta, structure, fasta_single_snp, structure_single_snp, write_singleDefault: 'fasta'
 
--t num_cpu_cores - The number of threads to run in parallel sections of code.
+-t num_cpu_cores - The number of threads to run in parallel sections of code. Default: 2
 
 -o output_dir - The absolute path to the output directory to contain the populations stacks output files and directories.
-
 
 USAGE
 }
