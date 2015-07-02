499cc6066961b179ffc180452dc34b00a2e1281d
cookeadmin <kevin5@ualberta.ca>
2015-07-02 11:56:59 -0600

    Updated refgen_stacks_analysis_pipeline.pl on 2015-07-02. Fixed issue where the -t gbs_fastq_type = fastq and was an untrimmed fastq file. The code would try to unlink the renamed file after it thought it was a trimmed fastq file with trimmed_offset in the filename. Also changed the refgen fasta parser so that the table of contents (toc) file parsing the fasta header properly

--- bin/refgen_stacks_analysis_pipeline.pl
+++ bin/refgen_stacks_analysis_pipeline.pl
@@ -279,7 +279,7 @@ sub convert_refgen_bwa_input_format{
                     }else{
                         die "Error: $_ is not in the correct format!";
                     }
-                }elsif($_ =~ m/^[ACGTNRYSWKM]+/i){
+                }elsif($_ =~ m/^[ACGTNRYSWKM]+/){
                     my $sequence = $_;
                     push(@{$fasta_seqs{$seq_counter}{"SEQUENCE"}}, $sequence);
                 }else{
