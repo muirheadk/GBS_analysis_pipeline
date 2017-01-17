0229354fd2efdeebd32db1209866b0ac6ef80b68
Kevin Muirhead <kmuirhead@Kevins-MacBook-Pro-2.local>
2017-01-16 23:25:39 -0700

    Fixed refgen_stacks1.44_install.sh . Script was missing cd  before the wget commands.

--- GBS_analysis_pipeline_v1.23/refgen_stacks1.44_install.sh
+++ GBS_analysis_pipeline_v1.23/refgen_stacks1.44_install.sh
@@ -3,17 +3,17 @@
 software_dir="$HOME/software"
 mkdir -p $software_dir
 
-cd $software_dir
-
 bwa_dir="$software_dir/bwa-0.7.15"
 samtools_dir="$software_dir/samtools-1.3.1"
 stacks_dir="$software_dir/stacks-1.44"
 
+cd $software_dir
 wget http://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.15.tar.bz2
 tar xvjf bwa-0.7.15.tar.bz2
 cd $bwa_dir
 make
 
+cd $software_dir
 wget https://sourceforge.net/projects/samtools/files/samtools/1.3.1/samtools-1.3.1.tar.bz2
 tar xvjf samtools-1.3.1.tar.bz2
 cd $samtools_dir
@@ -21,6 +21,7 @@ cd $samtools_dir
 make
 make install
 
+cd $software_dir
 wget http://catchenlab.life.illinois.edu/stacks/source/stacks-1.44.tar.gz
 tar xvzf stacks-1.44.tar.gz
 cd $stacks_dir
