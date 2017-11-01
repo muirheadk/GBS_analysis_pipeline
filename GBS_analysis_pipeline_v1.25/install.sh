b24e7bb5b358a855ef0b6263ed27293a27eef20c
Kevin Muirhead <kevmu@cedar5.cedar.computecanada.ca>
2017-10-31 19:43:50 -0700

    Added version 1.25 with installer script for easy installation of the pipeline and related tools on 2017-10-31.

--- GBS_analysis_pipeline_v1.25/install.sh
+++ GBS_analysis_pipeline_v1.25/install.sh
@@ -3,20 +3,22 @@
 software_dir="$HOME/software"
 mkdir -p $software_dir
 
-
-bwa_dir="$software_dir/bwa-0.7.15"
-samtools_dir="$software_dir/samtools-1.3.1"
-stacks_dir="$software_dir/stacks-1.47"
-
+# Download the github repository for the GBS_analysis_pipeline
 cd $software_dir
 git clone https://github.com/muirheadk/GBS_analysis_pipeline.git
 
+pipeline_dir="$software_dir/GBS_analysis_pipeline"
+
+# Install BWA version 0.7.15
+bwa_dir="$software_dir/bwa-0.7.15"
 cd $software_dir
 wget http://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.15.tar.bz2
 tar xvjf bwa-0.7.15.tar.bz2
 cd $bwa_dir
 make
 
+# Install Samtools version 1.3.1
+samtools_dir="$software_dir/samtools-1.3.1"
 cd $software_dir
 wget https://sourceforge.net/projects/samtools/files/samtools/1.3.1/samtools-1.3.1.tar.bz2
 tar xvjf samtools-1.3.1.tar.bz2
@@ -25,6 +27,8 @@ cd $samtools_dir
 make
 make install
 
+# Install Stacks Version 1.47
+stacks_dir="$software_dir/stacks-1.47"
 cd $software_dir
 wget http://catchenlab.life.illinois.edu/stacks/source/stacks-1.47.tar.gz
 tar xvzf stacks-1.47.tar.gz
@@ -32,4 +36,9 @@ cd $stacks_dir
 ./configure --prefix="$stacks_dir"
 make
 make install
+
+# Install Perl Modules from CPAN
+cpan String::Approx
+cpan Parallel::ForkManager
+
 
