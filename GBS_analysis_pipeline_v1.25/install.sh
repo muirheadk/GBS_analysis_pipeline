964529cea2c0c9be58a0b08a4dcddd8e7e6a068c
Kevin Muirhead <kevmu@cedar5.cedar.computecanada.ca>
2017-12-15 22:57:03 -0800

    Updated on 2017-12-15. Added PERL5LIB to .bashrc file

--- GBS_analysis_pipeline_v1.25/install.sh
+++ GBS_analysis_pipeline_v1.25/install.sh
@@ -9,6 +9,8 @@ git clone https://github.com/muirheadk/GBS_analysis_pipeline.git
 
 pipeline_dir="$software_dir/GBS_analysis_pipeline"
 
+echo "export PATH=$PATH:$pipeline_dir/GBS_analysis_pipeline_v1.25" >> $HOME/.bashrc
+
 # Install BWA version 0.7.15
 bwa_dir="$software_dir/bwa-0.7.15"
 cd $software_dir
@@ -16,6 +18,8 @@ wget http://downloads.sourceforge.net/project/bio-bwa/bwa-0.7.15.tar.bz2
 tar xvjf bwa-0.7.15.tar.bz2
 cd $bwa_dir
 make
+
+echo "export PATH=$PATH:$bwa_dir" >> $HOME/.bashrc
 
 # Install Samtools version 1.3.1
 samtools_dir="$software_dir/samtools-1.3.1"
@@ -27,6 +31,8 @@ cd $samtools_dir
 make
 make install
 
+echo "export PATH=$PATH:$samtools_dir/bin" >> $HOME/.bashrc
+
 # Install Stacks Version 1.44
 stacks_dir="$software_dir/stacks-1.44"
 cd $software_dir
@@ -37,8 +43,16 @@ cd $stacks_dir
 make
 make install
 
+echo "export PATH=$PATH:$stacks_dir/bin" >> $HOME/.bashrc
+
 # Install Perl Modules from CPAN
-cpan String::Approx
-cpan Parallel::ForkManager
+
+perl_module_dir="$HOME/perl5/lib/perl5"
+
+echo "export PERL5LIB=$PERLLIB:\"$perl_module_dir\"" >> $HOME/.bashrc
+
+source $HOME/.bashrc
+
+cpan install Parallel::Loops
 
 
